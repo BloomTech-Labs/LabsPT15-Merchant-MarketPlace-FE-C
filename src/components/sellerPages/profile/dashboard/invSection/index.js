05da34b395cf49e65858ed0819d8802107a7d390
Brian <lawmen218@yahoo.com>
2021-02-21 22:03:14 -0500

    delete this

--- src/components/sellerPages/profile/dashboard/invSection/index.js
+++ src/components/sellerPages/profile/dashboard/invSection/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
 import SmallItemCard from '../../../../common/cards/smallItem';
+import { useOktaAuth } from '@okta/okta-react/src/OktaContext';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-function InvSection() {
+import { fetchProducts } from '../../../../../state/actions';
+
+function InvSection({ inventory, getProductsStatus }) {
+  const { authState } = useOktaAuth();
+  useEffect(() => {
+    fetchProducts(authState);
+  }, []);
+
+  console.log(inventory);
   return (
     <>
       <h2>Inventory</h2>
       <h4>Details</h4>
-      <SmallItemCard headerText="Option Text" descText="Subtext" />
+      {inventory.map(item => (
+        <SmallItemCard headerText={item.itemName} descText={item.description} />
+      ))}
     </>
   );
 }
-export default InvSection;
+
+const mapStateToProps = state => ({
+  inventory: state.products.products,
+  getProductsStatus: state.products.getProductsStatus,
+});
+
+export default connect(mapStateToProps, { fetchProducts })(InvSection);
